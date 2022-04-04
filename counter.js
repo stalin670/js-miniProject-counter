af2a731966214b48149354a0a796c6398b8ad409
Tyrant670 <88823371+Tyrant670@users.noreply.github.com>
2022-04-04 12:27:05 +0530

    Add files via upload

---
+++ counter.js
@@ -0,0 +1,33 @@
+const counter = document.querySelector('#counter')
+const btns = document.querySelectorAll('.btn')
+
+
+let count = 0
+
+btns.forEach((btn) => {
+	btn.addEventListener('click', (data) => {
+		const styles = data.currentTarget.classList
+
+		if (styles.contains('increase')) {
+			count++
+		} else if (styles.contains('decrease')) {
+			count--
+		} else {
+			count = 0
+		}
+
+		if (count > 0) {
+			counter.style.color = 'green'
+		}
+
+		if (count < 0) {
+			counter.style.color = 'red'
+		}
+
+		if (count === 0) {
+			counter.style.color = 'grey'
+		}
+
+		counter.textContent = count
+	})
+})
